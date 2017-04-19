c10c727b59dbd0f0ca8e887a57f09dde5f640314
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-04-19 16:54:18 -0300

    Printando estrutura

--- API/public/javascripts/javascript.js
+++ API/public/javascripts/javascript.js
@@ -3,7 +3,7 @@ function filtroFilmes(){
 	$.get('https://tv-v2.api-fetch.website/movies/125?sort=last%20added&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000caneca/detalhada/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1> Duração: '+data[x].runtime+' minutos</h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000caneca/detalhada/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><p>Genre: '+data[x].genres+'</p><h1> Duration: '+data[x].runtime+' minutes</h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
 }
