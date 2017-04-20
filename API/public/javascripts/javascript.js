09357381e15eff214621a06cab084cefb92d0a55
BiancaDuarte <bianca.duarte@digitaldesk.com.br>
2017-04-20 08:21:26 -0300

    Falta página do produto

--- API/public/javascripts/javascript.js
+++ API/public/javascripts/javascript.js
@@ -3,7 +3,7 @@ function filtroFilmes(){
 	$.get('https://tv-v2.api-fetch.website/movies/125?sort=last%20added&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000caneca/detalhada/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><p>Genre: '+data[x].genres+'</p><h1> Duration: '+data[x].runtime+' minutes</h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><p>Genre: '+data[x].genres+'</p><h1> Duration: '+data[x].runtime+' minutes</h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
 }
@@ -13,7 +13,7 @@ function filtroSeries(){
 	$.get('https://tv-v2.api-fetch.website/shows/24?sort=name&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000caneca/detalhada/'+data[x].id+'"><img src='+data[x].images.poster+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'"><img src='+data[x].images.poster+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
 }
@@ -23,7 +23,7 @@ function filtroAnimes(){
 	$.get('https://tv-v2.api-fetch.website/animes/12?sort=name&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000caneca/detalhada/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
 }
@@ -43,4 +43,38 @@ function actions(){
 }
 $(document).ready(function () {
 	actions();
+
+	$('#txt-search').keyup(function(){
+	var searchField = $(this).val();
+	if(searchField === '')  {
+		$('#resultados-busca').hide();
+		$('#resultados-busca').html('');
+		return;
+	}
+
+	var regex = new RegExp(searchField, "i");
+	var resultados = '<div class="row">';
+	var count = 1;
+	$.get('https://tv-v2.api-fetch.website/movies/125?sort=last%20added&order=1','https://tv-v2.api-fetch.website/animes/12?sort=name&order=1', function(data) {
+	$('#txt-search').show();	
+	$('#resultados-busca').show();
+	for (x in data) {
+		var nome = data[x].title;
+			if (nome.search(regex) != -1){
+				resultados += '<div class="col-md-12 well">';
+				resultados += '<div class="col-md-3"><img class="img-responsive" src="'+data[x].images.banner+'"/></div> <a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'">';
+				resultados += '<div class="col-md-9">';
+				resultados += '<h5>' + nome + '</h5>';
+				resultados += '<p>' + data[x].synopsis + '</p>';
+				resultados += '<h4> Duration: '+data[x].runtime+' minutes </h4>'
+				resultados += '<h4> Genre: '+data[x].genres+ '</h4>'
+				resultados += '<h3>' + data[x].year + '</h3>'
+				resultados += '</div>';
+				resultados += '</div>';
+			}
+	}
+		resultados += '</div>';
+		$('#resultados-busca').html(resultados);
+	});
+});
 });
--- API/routes/quadroselecionado.js
+++ API/routes/quadroselecionado.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/:cod', function (req, res){
-	fs.readFile(__dirname + '/../db/quadros.json', 'utf8', function(err, data){
+	fs.readFile(__dirname + 'https://tv-v2.api-fetch.website/movies/125?sort=last%20added&order=1', 'utf8', function(err, data){
 	data = JSON.parse(data);
 	var posicao = 0;
-	for(i in data.quadros){
-		if(data.quadros[i].Código==req.params.cod){
-			posicao = i;
+	for(x in data){
+		if(data[x].id==req.params.cod){
+			posicao = x;
 		}
 	}
 	res.render('produtoselecionado', { dados:data, cod:posicao });
