8f957154e9c566694e4eab9143c8b13aa264290e
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-04-20 13:14:55 -0300

    searchbar

--- API/public/javascripts/javascript.js
+++ API/public/javascripts/javascript.js
@@ -1,9 +1,11 @@
+var flag=0, ID, TIPO;
+
 function filtroFilmes(){
 	$('#quadros').empty();
 	$.get('https://tv-v2.api-fetch.website/movies/125?sort=last%20added&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><p>Genre: '+data[x].genres+'</p><h1> Duration: '+data[x].runtime+' minutes</h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="#modal1"><img data-tipo="filmes" data-id="'+data[x]._id+'" class="clica" src='+data[x].images.banner+'><figcaption><p> Rating: '+data[x].rating.percentage+' </p></figcaption></a></figure><div><p>Genre: '+data[x].genres+'</p><h1> Duration: '+data[x].runtime+' minutes</h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
 }
@@ -13,7 +15,7 @@ function filtroSeries(){
 	$.get('https://tv-v2.api-fetch.website/shows/24?sort=name&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'"><img src='+data[x].images.poster+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="#modal1"><img data-tipo="series" data-id="'+data[x]._id+'" class="clica" src='+data[x].images.poster+'><figcaption><p> Rating: '+data[x].rating.percentage+' </p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
 }
@@ -23,9 +25,37 @@ function filtroAnimes(){
 	$.get('https://tv-v2.api-fetch.website/animes/12?sort=name&order=1', function(data) {
 		console.log(data);
 		for(var x=0; x<50; x++){
-			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'"><img src='+data[x].images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
+			$('#quadros').append('<div class="col-md-4 imagem"><h2>'+data[x].title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="#modal1"><img data-tipo="animes" data-id="'+data[x]._id+'" class="clica" src='+data[x].images.banner+'><figcaption><p> Rating: '+data[x].rating.percentage+' </p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data[x].year+'</p></div></div></p></div>');
 		}
 	});
+}
+
+function printProduto(){
+	console.log('Função produto');
+	$('#produto').empty();
+	if(flag==1){
+		console.log('Flag 1');
+		$.get('https://tv-v2.api-fetch.website/movie/'+ID, function(data) {
+			console.log(data);
+				$('#produto').append('<div class="row"><div class="col-md-12 tituloModal"><h2>'+data.title+'</h2></div></div><div class="row"><div class="col-md-5 tamanho"><img src='+data.images.banner+'></div><div class="col-md-7"><p>Released: '+data.year+'</p><p>Genres: '+data.genres+'</p><p>Rating: '+data.rating.percentage+'</p><p>Duration: '+data.runtime+' minutes</p><p>Synopsis: '+data.synopsis+'</p></div></div>');
+		});
+	}
+
+	else if(flag==2){
+		console.log('Flag 2');
+		$.get('https://tv-v2.api-fetch.website/show/'+ID, function(data) {
+			console.log(data);
+				$('#produto').append('<div class="col-md-4 imagem"><h2>'+data.title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:3100/quadro/detalhado/'+data.id+'"><img src='+data.images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data.year+'</p></div></div></p></div>');
+		});
+	}
+
+	else if(flag==3){
+		console.log('Flag 3');
+		$.get('https://tv-v2.api-fetch.website/anime/'+ID, function(data) {
+			console.log(data);
+				$('#produto').append('<div class="col-md-4 imagem"><h2>'+data.title+'</h2><p><div class="grid"><figure class="effect-zoe"><a href="http://localhost:3100/quadro/detalhado/'+data.id+'"><img src='+data.images.banner+'><figcaption><p class="icon-links"><a href="#"><i class="material-icons small"> shopping_cart</i></a><a href="#"><i class="material-icons small"> star</i></a></p></figcaption></a></figure><div><h1>A partir de R$ </h1><p>'+data.year+'</p></div></div></p></div>');
+		});
+	}
 }
 
 function actions(){
@@ -40,9 +70,23 @@ function actions(){
 	$('#animes').click(function(){
 		filtroAnimes();
 	});
+	
+	$('#quadros').on('click', ".clica", function(){
+		ID = $(this).data("id");
+		TIPO = $(this).data("tipo");
+		if(TIPO=="filmes")
+			flag=1;
+		if(TIPO=="series")
+			flag=2;
+		if(TIPO=="animes")
+			flag=3;
+		printProduto();
+	});
 }
 $(document).ready(function () {
 	actions();
+	printProduto();
+	$('.modal').modal();
 
 	$('#txt-search').keyup(function(){
 	var searchField = $(this).val();
@@ -62,7 +106,7 @@ $(document).ready(function () {
 		var nome = data[x].title;
 			if (nome.search(regex) != -1){
 				resultados += '<div class="col-md-12 well">';
-				resultados += '<div class="col-md-3"><img class="img-responsive" src="'+data[x].images.banner+'"/></div> <a href="http://localhost:60000/quadro/detalhado/'+data[x].id+'">';
+				resultados += '<div class="col-md-3"><img class="img-responsive" src="'+data[x].images.banner+'"/></div> <a href="#modal1">';
 				resultados += '<div class="col-md-9">';
 				resultados += '<h5>' + nome + '</h5>';
 				resultados += '<p>' + data[x].synopsis + '</p>';
--- API/routes/quadroselecionado.js
+++ API/routes/quadroselecionado.js
@@ -3,18 +3,9 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/:cod', function (req, res){
-	fs.readFile(__dirname + 'https://tv-v2.api-fetch.website/movies/125?sort=last%20added&order=1', 'utf8', function(err, data){
-	data = JSON.parse(data);
-	var posicao = 0;
-	for(x in data){
-		if(data[x].id==req.params.cod){
-			posicao = x;
-		}
-	}
-	res.render('produtoselecionado', { dados:data, cod:posicao });
-	
-			res.end();
-	});
+
+	res.render('produtoselecionado');
+	res.end();
 
 });
 
